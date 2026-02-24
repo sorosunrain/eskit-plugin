293d9263398ca6268c416ccc6a3270f42319f94c
weipeng <weipeng@bftv.com>
2026-02-24 14:56:33 +0800

    init

---
+++ app/src/main/java/tv/huan/demo/App.java
@@ -0,0 +1,16 @@
+package tv.huan.demo;
+
+import android.app.Application;
+
+/**
+ * <br>
+ *
+ * <br>
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
